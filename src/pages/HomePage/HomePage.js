dee039472d28e472643e56c0f925b167735ddff3
Lucca Viccini <lucca.viccini@engenharia.ufjf.br>
2023-01-19 15:56:44 -0300

    feat: homePage Log styled

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -5,15 +5,48 @@ import {
   LogBox,
   ContainerHeader,
   GreetingMsg,
+  Entry,
+  ContainerSaldo
 } from "./style";
 import Logo from "../../assets/VectorOut.svg";
 import Plus from "../../assets/plus.svg";
 import Minus from "../../assets/minus.svg";
 //import { useNavigate } from "react-router-dom";
 
+
+
 export default function HomePage() {
   //const navigate = useNavigate();
+  const inOut = 1;
+  const income = -10;
+  let userBalance = 0;
 
+  const mylist = [
+    {date: "11/02", description: "Descrição", value: "50,00", inOut: 0},
+    {date: "11/02", description: "Descrição", value: "50,00", inOut: 0},
+    {date: "11/02", description: "Descrição", value: "50,00", inOut: 1},
+  ];
+
+  mylist.forEach((item) => {
+    
+    
+    let myMoney = Number(item.value.replace(",", "."));
+       
+
+    
+    
+    if (item.inOut === 1) {
+      
+      userBalance += myMoney;
+      
+    }
+    else{
+      userBalance -= myMoney;
+    }
+  });
+  userBalance = userBalance.toFixed(2);
+  userBalance = userBalance.replace(".", ",");
+  
   return (
     <Container>
       <ContainerHeader>
@@ -21,7 +54,21 @@ export default function HomePage() {
         <img src={Logo} alt="Logo MyWallet" />
       </ContainerHeader>
       <LogBox>
-        Não há registros de <br /> entrada ou saída
+
+        {mylist.map((item, index) => (
+          <Entry key={index}>
+            <span className="myclass">
+              {item.date}
+              <span className="description"> {item.description} </span>
+            </span>
+            <span className={item.inOut ? "in" : "out"}> {item.value} </span>
+          </Entry>
+        ))}
+        
+      <ContainerSaldo userBalance={userBalance} >
+        <span className="saldoTxt">Saldo</span>
+        <span className="saldoVal">{userBalance}</span>
+      </ContainerSaldo>
       </LogBox>
       <ContainerBtns>
         <Btn to="/nova-entrada">
@@ -39,3 +86,5 @@ export default function HomePage() {
     </Container>
   );
 }
+
+
--- src/pages/HomePage/style.js
+++ src/pages/HomePage/style.js
@@ -46,22 +46,53 @@ export const LogBox = styled.div`
   background: #ffffff;
   border-radius: 5px;
   display: flex;
+  flex-direction: column;
   align-items: center;
-  justify-content: center;
-
+  justify-content: flex-start;
   font-family: "Raleway";
   font-style: normal;
   font-weight: 400;
   font-size: 20px;
   line-height: 23px;
   text-align: center;
-
+  padding-top: 23px;
   color: #868686;
+  position: relative;
+`;
+
+export const ContainerSaldo = styled.div`
+  width: calc(100% - 40px);
+  display: flex;
+  justify-content: space-between;
+  position: absolute;
+  bottom: 5px;
+
+  .saldoTxt {
+    font-family: "Raleway";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 17px;
+    line-height: 20px;
+    color: #000000;
+  }
+
+  .saldoVal {
+    font-family: "Raleway";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 17px;
+    line-height: 20px;
+    /* identical to box height */
+
+    text-align: right;
+
+    color: ${(props) => (props.userBalance > 0  ? "#03ac00" : "#c70000")}};
+  
 `;
 
 export const ContainerHeader = styled.div`
   display: flex;
-  width: calc(100% );
+  width: calc(100%);
   justify-content: space-between;
   align-items: flex-start;
   margin-bottom: 13px;
@@ -75,4 +106,49 @@ export const GreetingMsg = styled.h1`
   font-weight: 700;
   font-size: 26px;
   line-height: 31px;
+`;
+
+export const Entry = styled.div`
+  width: calc(100% - 40px);
+  height: 30px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .out {
+    color: #c70000;
+    font-family: "Raleway";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 19px;
+    text-align: right;
+  }
+  .in {
+    color: #03ac00;
+    font-family: "Raleway";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 19px;
+    text-align: right;
+  }
+  .description {
+    font-family: "Raleway";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 19px;
+    color: #000000;
+    margin-left: 5px;
+  }
+
+  .date {
+    font-family: "Raleway";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 19px;
+    color: #c6c6c6;
+  }
 `;
