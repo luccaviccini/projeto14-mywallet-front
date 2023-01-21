40f1c7900008d18c6830e7591681ebab668c9f70
Lucca Viccini <lucca.viccini@engenharia.ufjf.br>
2023-01-21 01:12:09 -0300

    feat: getting entries from db and showing to user

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -19,35 +19,11 @@ import { useContext, useEffect, useState } from "react";
 
 
 export default function HomePage() {
-  //const navigate = useNavigate();
   
-  // let userBalance = 0;
-
-  // const mylist = [
-  //   {date: "11/02", description: "Descrição", value: "50,00"},
-  //   {date: "11/02", description: "Descrição", value: "50,00"},
-  //   {date: "11/02", description: "Descrição", value: "50,00"},
-  // ];
-
-  
-
-  // mylist.forEach((item) => {   
-    
-  //   let myMoney = Number(item.value.replace(",", "."));     
-  //   console.log(myMoney)
-  //   userBalance += myMoney;
-
-    
-  // });
-  // userBalance = userBalance.toFixed(2);
-  
-  // console.log(userBalance);
-
-  //get entries from db
   const { user } = useContext(UserContext);
   const [userEntries, setUserEntries] = useState([]);
 
-  useEffect(() => {
+  function getEntriesList() {
     apiEntry
       .getEntries(user.token)
       .then((res) => {
@@ -55,12 +31,12 @@ export default function HomePage() {
         setUserEntries(res.data);
       })
       .catch((err) => {
-        console.log(err.response.data);
+        alert(err.response.data.message);
       });
-  }, []);
+  }
 
-  
-
+  useEffect(getEntriesList, []);
+  console.log(userEntries.entries);
    
   let userBalance = userEntries.balance;
 
@@ -74,7 +50,7 @@ export default function HomePage() {
         </Link>
       </ContainerHeader>
       <LogBox>
-        {userEntries.length > 0?
+        {userEntries.entries.length > 0?
         userEntries.entries.map((entry, index) => (
           <Entry key={index} value={entry.value} type={entry.type}>
             <span className="myclass">
@@ -86,15 +62,7 @@ export default function HomePage() {
         ))        
         : <h1>Não há registros de entrada ou saída</h1>}
 
-        {/* {userEntries.entries.map((entry, index) => (
-          <Entry key={index} value={entry.value} type={entry.type}>
-            <span className="myclass">
-              {entry.date}
-              <span className="description"> {entry.description} </span>
-            </span>
-            <span className="inOut"> {entry.value} </span>
-          </Entry>
-        ))} */}
+       
 
         <ContainerSaldo userBalance={userBalance}>
           <span className="saldoTxt">Saldo</span>
